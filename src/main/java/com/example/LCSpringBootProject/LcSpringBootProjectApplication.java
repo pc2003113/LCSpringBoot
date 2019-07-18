1dc5d12526807e14ebd0e56d0af45e94bb33fcc9
PriyaC <priyacjose@gmail.com>
2019-07-17 19:46:21 -0500

    your awesomely descriptive, yet pithy commit message goes here

---
+++ src/main/java/com/example/LCSpringBootProject/LcSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.LCSpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcSpringBootProjectApplication.class, args);
+	}
+
+}
